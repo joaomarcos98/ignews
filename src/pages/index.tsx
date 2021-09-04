2275a3518e87b2b3d02990e62337ba3ff5e9c5da
Joao <joomarcos.belanga98@gmail.com>
2021-09-04 09:42:33 -0300

    Dinamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import styled from "../styles/home.module.scss"
+import styled from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    
-    <h1 className={styled.title}>Hello world!</h1>
-      
+
+    <>
+      <Head>
+        <title>Início | Ig.news</title>
+      </Head>
+      <h1 className={styled.title}>Hello world!</h1>
+    </>
+
   )
 }
