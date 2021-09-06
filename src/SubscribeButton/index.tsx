7ee3320ec4ddb00fbc88302b2e03d409ae84dcd4
Joao <joomarcos.belanga98@gmail.com>
2021-09-06 09:56:37 -0300

    Subcribe button

---
+++ src/SubscribeButton/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.scss"
+
+export function SubcribeButton() {
+    return (
+        <button
+            type="button"
+            className={styles.subcribeButton}
+        >
+            Subscribe now
+        </button>
+    )
+}
