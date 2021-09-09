77ac7c48c77a7b9adc901f58175670f6f1e93e94
Joao <joomarcos.belanga98@gmail.com>
2021-09-09 14:57:17 -0300

    Adding route payment

--- src/SubscribeButton/index.tsx
+++ src/SubscribeButton/index.tsx
@@ -1,5 +1,9 @@
 import { signIn, useSession } from "next-auth/client";
+import { api } from "../services/api";
+import { getStripeJs } from "../services/stripe-js";
+
 import styles from "./styles.module.scss";
+
 
 interface SubscribeButtonProps {
     priceId: string
@@ -9,14 +13,29 @@ export function SubcribeButton({ priceId }: SubscribeButtonProps) {
 
     const [session] = useSession();
 
-    function handleSubcribe() {
+    async function handleSubcribe() {
 
         if (!session) {
             signIn("github")
             return;
         }
 
+        try {
+            const response = await api.post("/subscribe")
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+
+        } catch (err) {
+            alert(err.message)
+        }
+
     }
+
+
     return (
         <button
             type="button"
