62f9979ecf07ec8dc4d0339433eb5b5c9a6c3f7d
Joao <joomarcos.belanga98@gmail.com>
2021-09-10 16:43:09 -0300

    WebHook fixed

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubcription";
+
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -29,27 +31,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (req.method === "POST") {
         const buf = await buffer(req);
+
         const secret = req.headers["stripe-signature"]
 
-        let event: Stripe.Event;
+        let event: Stripe.Event
 
         try {
-            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
         } catch (err) {
             return res.status(400).send(`Webhook error: ${err.message}`);
         }
 
         const { type } = event
 
-        if (!relevantEvents.has(type)) {
+        if (relevantEvents.has(type)) {
             console.log("Evento recebido", event);
 
         }
 
         res.json({ received: true })
     } else {
-        res.setHeader("Allow", "POST");
-        res.status(405).end("Method not allowed");
+        res.setHeader("Allow", "POST")
+        res.status(405).end("Method not allowed")
     }
+
+
 
 }
