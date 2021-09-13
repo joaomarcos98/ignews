7154328439fc0a66892096ed9e983ce1a26e67ec
Joao <joomarcos.belanga98@gmail.com>
2021-09-13 11:37:18 -0300

    Adding more event listeners

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -24,7 +24,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    "checkout.session.completed"
+    "checkout.session.completed",
+    "customer.subscription.updated",
+    "customer.subscription.deleted",
+
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -45,16 +48,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const { type } = event
 
         if (relevantEvents.has(type)) {
-            
+
             try {
                 switch (type) {
-                    case "checkout.session.completed":                        
+
+                    case "customer.subscription.updated":
+                    case "customer.subscription.deleted":
+                        const subscription = event.data.object as Stripe.Subscription
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false
+                        )
+
+
+                    case "checkout.session.completed":
 
                         const checkoutSession = event.data.object as Stripe.Checkout.Session
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
+                            checkoutSession.customer.toString(),
+                            true,
                         )
 
                         break
