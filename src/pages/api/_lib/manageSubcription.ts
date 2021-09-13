fb59ef1bfc1e4f9e84b8d76e0e4bb0e8462095f5
Joao <joomarcos.belanga98@gmail.com>
2021-09-13 10:56:01 -0300

    Save transaction in FaunaDB

--- src/pages/api/_lib/manageSubcription.ts
+++ src/pages/api/_lib/manageSubcription.ts
@@ -6,7 +6,33 @@ export async function saveSubscription(
     subscriptionId: string,
     customerId: string,
 ) {
-    console.log(subscriptionId, customerId, "certoo");
 
+    const userRef = await fauna.query(
+        q.Select(
+            "ref",
+            q.Get(
+                q.Match(
+                    q.Index("user_by_stripe_costumer_id"),
+                    customerId,
+                )
+            )
+        )
+    )
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id
+    }
+
+    await fauna.query(
+        q.Create(
+            q.Collection("subscriptions"),
+            { data: subscriptionData }
+        )
+    )
 }
 
