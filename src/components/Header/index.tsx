eba1a8946f7cbc251d348981c62cff0c5a9404c4
Joao <joomarcos.belanga98@gmail.com>
2021-09-13 16:17:45 -0300

    Header Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import { SignInButton } from "../SignInButton";
+import Link from "next/link";
 import styles from "./styles.module.scss";
-import { signIn } from "next-auth/client";
+import { useRouter } from "next/router";
+import { ActiveLink } from "../ActiveLink";
 
 
 export function Header() {
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="Ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a >Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
